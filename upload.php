1a56ef92e0ef4c5bc162188ef8c96da77a446eb3
Mr.Gamer <nicklaus.vega21@gmail.com>
2022-09-16 12:40:11 -0500

    more debugging

--- upload.php
+++ upload.php
@@ -4,11 +4,10 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-echo $target_file
-echo $imageFileType
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
+    echo $target_file
+    echo $imageFileType
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
